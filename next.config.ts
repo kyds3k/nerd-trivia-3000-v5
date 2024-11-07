3e786e50e3780973f8a4dfaaec875173df29dd26
Adam Darby <kyds3k@gmail.com>
2024-11-07 17:01:50 +0300

    Fixed some merges, NextUI should be in now

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
+	experimental: {
+		optimizePackageImports: ["@nextui-org/react"],
+		parallelServerCompiles: true,
+		ppr: true,
+		webpackBuildWorker: true,
+	},
+	typescript: {
+		ignoreBuildErrors: true,
+	},
 };
 
 export default nextConfig;
